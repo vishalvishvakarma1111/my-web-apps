9aa258625c53189e7c75a57d4e83a1a71e720cbb
vishal_mac <psspl.trainee55@outlook.com>
2023-01-13 12:45:42 +0530

    added whatsapp ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:native_app/whatsapp_ui/ui/mainscreen.dart';
 
-import 'dashboard/dashboard_view.dart';
+import 'crickbuzz/dashboard/dashboard_view.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
@@ -14,12 +16,99 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      getPages: [
+        GetPage(
+          name: "/whatsapp-view",
+          page: () => const WhatsAppView(),
+        ),
+        GetPage(
+          name: "/dashboard-crb",
+          page: () => const CrickBuzzDashBoardView(),
+        ),
+      ],
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const DashboardView(),
+          primarySwatch: Colors.blue,
+          scrollbarTheme: const ScrollbarThemeData().copyWith(
+            thumbVisibility: MaterialStateProperty.all(true),
+            thickness: MaterialStateProperty.all(10),
+          )),
+
+      ///   home: const DashboardView(),/// crickbuzz
+      ///    home:   Chat(), /// chat gpt
+      home: const MainScreen(),
+
+      /// chat gpt
     );
+  }
+}
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(image: AssetImage("assets/images/bg.jpg"), fit: BoxFit.fill),
+          ),
+          padding: const EdgeInsets.all(20),
+          height: double.maxFinite,
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              buildBtn(() {
+                Get.toNamed('/dashboard-crb');
+              }, "CrickBuzz"),
+              const SizedBox(width: 20, height: 20),
+              buildBtn(() {
+                Get.toNamed('/whatsapp-view');
+              }, "Whatsapp"),
+            ],
+          )
+
+          /*GridView.count(
+          crossAxisCount: 4,
+          childAspectRatio: 3,
+          mainAxisSpacing: 10,
+          crossAxisSpacing: 10,
+          children: [
+            buildBtn(() {
+              Get.toNamed('/dashboard-crb');
+            }, "CrickBuzz"),
+            buildBtn(() {
+              Get.toNamed('/whatsapp-view');
+
+            }, "Whatsapp"),
+          ],
+        ),*/
+          ),
+    );
+  }
+
+  buildBtn(Function() onPressed, String title) {
+    return LayoutBuilder(
+        builder: (context, c) => ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.all(Get.width * .04),
+                  backgroundColor: Colors.orangeAccent[100],
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30))),
+              onPressed: onPressed,
+              child: Row(
+                children: [
+                  Text(
+                    title,
+                    style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white),
+                  ),
+                  const Icon(
+                    Icons.arrow_forward_ios,
+                    color: Colors.white,
+                  )
+                ],
+              ),
+            ));
   }
 }
--- lib/util/const.dart
+++ lib/util/const.dart
@@ -2,7 +2,14 @@ import 'package:flutter/material.dart';
 
 ///   colors
 Color bgColor = const Color(0xFFE3E6E3);
- Color green = const Color(0xFF31725C);
+Color green = const Color(0xFF31725C);
+Color darkGreen = const Color(0xFF075e54);
+Color greenWtApp = const Color(0xFF128c7e);
+Color lightGreen = const Color(0xFF25d366);
+Color sightGreen = const Color(0xFFdcf8c6);
+Color blue = const Color(0xFF34b7f1);
+Color cream = const Color(0xFFece5dd);
+Color wtAppbar = const Color(0xFFF0F2F5);
 
 ///  string
 ///
